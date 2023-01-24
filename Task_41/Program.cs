d9234b8c751b21b2f247b59f39edf15c0687874b
Natalia <natalia-9988@yandex.ru>
2023-01-24 16:59:12 +0500

    Task _41

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -2,7 +2,7 @@
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3*/
 
-Console.Write("Введите колличество чисел: ");
+/*Console.Write("Введите колличество чисел: ");
 int n = int.Parse(Console.ReadLine()!);
 int count = 0;
 for (int i = 0; i < n; i++)
@@ -12,4 +12,29 @@ for (int i = 0; i < n; i++)
     if (x > 0)
         count++;
 }
-Console.WriteLine($"Положительных чисел: {count}");
+Console.WriteLine($"Положительных чисел: {count}");*/
+
+int getIntFromUser(string message)  
+{
+    Console.WriteLine(message);
+    int userInt = int.Parse(Console.ReadLine()!);
+    return userInt;
+}
+
+int getNum( int end)
+{
+    int count = 0;
+    
+for (int i = 0; i < end; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int x = int.Parse(Console.ReadLine()!);
+    if (x > 0)
+        count++;
+}
+return count;
+}
+
+int M = getIntFromUser( "Введите количество чисел :");
+int Sum = getNum(M);
+Console.WriteLine($"Положительных чисел: {Sum}");
